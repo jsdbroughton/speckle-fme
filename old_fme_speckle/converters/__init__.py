7a5631f551d5b72ccf27a51ea7945ff2a67c618f
Jonathon Broughton <jonathon@stardotbmp.com>
2026-07-25 11:27:01 +0200

    docs: delete unused
    Removed to streamline project content.

--- old_fme_speckle/converters/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Initial conversions will be a base FME Feature with Speckle display mesh."""
-
-from fme_speckle.converters.to_native import convert_to_native
-
--- tests/logger.py
+++
@@ -1,9 +0,0 @@
-class TestLogger:
-  def __init__(self):
-    self.messages = []
-
-  def logMessageString(self, message):
-    self.messages.append(message)
-
-  def get_messages(self):
-    return self.messages
